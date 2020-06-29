aab52902f18cfcdb5c949c366154088e84b87655
Christiano0407 <placebo0704@gmail.com>
2020-06-29 09:17:21 -0500

    omg-fin

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,23 @@
 const modal = document.querySelector(".modal");
 const previous = document.querySelectorAll(".gallery img");
 const original = document.querySelector(".fluid-img");
-const imgText = document.querySelector(".caption");
+const caption = document.querySelector(".caption");
 
 previous.forEach((preview) => {
     preview.addEventListener("click", () => {
         modal.classList.add("open");
+        original.classList.add("open");
+
+        const originalSrc = preview.getAttribute("data-original");
+        original.src = `../img/${originalSrc}`;
+        const altText = preview.alt;
+        caption.textContent = altText;
     });
 });
+
+modal.addEventListener("click", (e) => {
+if(e.target.classList.contains("modal")){
+    modal.classList.remove("open");
+    original.classList.remove("open");
+}
+})
